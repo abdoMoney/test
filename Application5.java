3eff13f731d2b0c5a2d0c4e8827b9729d071235f
abdoMoney <abdelati.baala92@gmail.com>
2024-01-14 17:31:31 +0100

    addition button in café ....

--- Application5.java
+++ Application5.java
@@ -1,7 +1,7 @@
 class Application5
 {  static void main(String[ ] args)
      { // recherche séquentielle dans un tableau
-        int [ ] table= {12,-5,7,8,-6,6,4,78,2};
+        int [ ] table= {12,-5,7,8,-6,6,4,78,2,44};
         byte elt = 4;
         short i ;
         for ( i = 0 ; i<8 ; i++ )
@@ -10,7 +10,7 @@ class Application5
       }
    static void afficher (int rang , long val)
    { if (rang == 8)
-        System.out.println("valeur : "+val+" pas trouvée.");
+        System.out.println("valeur : "+val+" pas trouvée.////////////////");
       else
         System.out.println("valeur : "+val+" trouvée au rang :"+ rang);
    }
